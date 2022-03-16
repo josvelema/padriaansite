c45bc68f9ad08a554f974f729d9d0c447876eb60
josvelema <volumensolutions@gmail.com>
2022-03-16 17:56:25 +0100

    added view per category in admin media page

--- admin/allmedia.php
+++ admin/allmedia.php
@@ -1,38 +1,86 @@
 <?php
 include 'main.php';
+
 // Delete the selected media
 if (isset($_GET['delete'])) {
     $stmt = $pdo->prepare('SELECT * FROM media WHERE id = ?');
-    $stmt->execute([ $_GET['delete'] ]);
+    $stmt->execute([$_GET['delete']]);
     $media = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($media['thumbnail']) {
         unlink('../' . $media['thumbnail']);
     }
     unlink('../' . $media['filepath']);
     $stmt = $pdo->prepare('DELETE m, mc FROM media m LEFT JOIN media_categories mc ON mc.media_id = m.id WHERE m.id = ?');
-    $stmt->execute([ $_GET['delete'] ]);
+    $stmt->execute([$_GET['delete']]);
     header('Location: allmedia.php');
     exit;
 }
 // Approve the selected media
 if (isset($_GET['approve'])) {
     $stmt = $pdo->prepare('UPDATE media SET approved = 1 WHERE id = ?');
-    $stmt->execute([ $_GET['approve'] ]);
+    $stmt->execute([$_GET['approve']]);
     header('Location: allmedia.php');
     exit;
 }
 // SQL query that will retrieve all the media from the database ordered by the ID column
-$stmt = $pdo->prepare('SELECT * FROM media ORDER BY id DESC');
-$stmt->execute();
-$media = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
-<?=template_admin_header('All Media', 'allmedia')?>
 
-<h2>All Media</h2>
+if (isset($_POST['viewCat'])) {
+    if ($_POST['viewCat'] !== 0) {
+        $stmt = $pdo->prepare('SELECT title FROM categories WHERE id = ? ');
+        $stmt->bindParam(1, $_POST['viewCat'], PDO::PARAM_INT);
+        $stmt->execute();
+        $catTitle = $stmt->fetchColumn();
+
+        $stmt = $pdo->prepare('SELECT * FROM media m JOIN media_categories mc ON mc.media_id = m.id AND mc.category_id = ? ORDER BY m.id DESC ');
+        // JOIN media_categories mc ON mc.media_id = m.id AND mc.category_id = :category' : 
+        $stmt->bindParam(1, $_POST['viewCat'], PDO::PARAM_INT);
+        $stmt->execute();
+        // var_dump("<pre>",$stmt,"</pre>");
+        $media = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+} else {
+
+    $stmt = $pdo->prepare('SELECT * FROM media ORDER BY id DESC');
+    $stmt->execute();
+    $media = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+?>
+
+
+<?= template_admin_header('All Media', 'allmedia') ?>
 
 <div class="links">
     <a href="media.php">Create Media</a>
 </div>
+<div class="rj-form-admin">
+    <form action="" method='post'>
+        <?php
+        $stmt = $pdo->prepare('SELECT * FROM categories ORDER BY id DESC');
+        $stmt->execute();
+        $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <select class="form-control" name="viewCat" id="">
+            <option value=0>Select Category</option>
+            <?php foreach ($categories as $category) : ?>
+                <option value=<?= $category['id'] ?>><?= $category['title'] ?></option>
+            <?php endforeach; ?>
+        </select>
+        <input type="submit" name="submit" class="btn btn-success" value="View Category">
+<a href="allmedia.php" class="btn btn-primary">View All Media</a>
+    </form>
+</div>
+
+<h2>Viewing 
+<?php 
+if (isset($catTitle)) {
+    echo " " . $catTitle;
+} else {
+    echo " all media.";
+}
+?>
+</h2>
+
 
 <div class="content-block">
     <div class="table">
@@ -51,35 +99,35 @@ $media = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 </tr>
             </thead>
             <tbody>
-                <?php if (empty($media)): ?>
-                <tr>
-                    <td colspan="8" style="text-align:center;">There are no recent media files</td>
-                </tr>
-                <?php else: ?>
-                <?php foreach ($media as $m): ?>
-                <tr>
-                    <td><?=htmlspecialchars($m['title'], ENT_QUOTES)?></td>
-                    <td><?= $m['year'] ?></td>
-                    <td><?= $m['fnr'] ?></td>
+                <?php if (empty($media)) : ?>
+                    <tr>
+                        <td colspan="8" style="text-align:center;">There are no recent media files</td>
+                    </tr>
+                <?php else : ?>
+                    <?php foreach ($media as $m) : ?>
+                        <tr>
+                            <td><?= htmlspecialchars($m['title'], ENT_QUOTES) ?></td>
+                            <td><?= $m['year'] ?></td>
+                            <td><?= $m['fnr'] ?></td>
 
-                    <td class="responsive-hidden"><?=nl2br(htmlspecialchars($m['description'], ENT_QUOTES))?></td>
-                    <td><a href="../<?=$m['filepath']?>" target="_blank">View</a></td>
-                    <td class="responsive-hidden"><?=$m['type']?></td>
-                    <td><?=$m['approved']?'Yes':'No'?></td>
-                    <td class="responsive-hidden"><?=date('F j, Y H:ia', strtotime($m['uploaded_date']))?></td>
-                    <td>
-                        <a href="media.php?id=<?=$m['id']?>" class="rj-action-edit">Edit</a>
-                        <a href="allmedia.php?delete=<?=$m['id']?>" class="rj-action-del">Delete</a>
-                        <?php if (!$m['approved']): ?>
-                        <a href="allmedia.php?approve=<?=$m['id']?>">Approve</a>
-                        <?php endif; ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
+                            <td class="responsive-hidden"><?= nl2br(htmlspecialchars($m['description'], ENT_QUOTES)) ?></td>
+                            <td><a href="../<?= $m['filepath'] ?>" target="_blank">View</a></td>
+                            <td class="responsive-hidden"><?= $m['type'] ?></td>
+                            <td><?= $m['approved'] ? 'Yes' : 'No' ?></td>
+                            <td class="responsive-hidden"><?= date('F j, Y H:ia', strtotime($m['uploaded_date'])) ?></td>
+                            <td>
+                                <a href="media.php?id=<?= $m['id'] ?>" class="rj-action-edit">Edit</a>
+                                <a href="allmedia.php?delete=<?= $m['id'] ?>" class="rj-action-del">Delete</a>
+                                <?php if (!$m['approved']) : ?>
+                                    <a href="allmedia.php?approve=<?= $m['id'] ?>">Approve</a>
+                                <?php endif; ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
                 <?php endif; ?>
             </tbody>
         </table>
     </div>
 </div>
 
-<?=template_admin_footer()?>
+<?= template_admin_footer() ?>
