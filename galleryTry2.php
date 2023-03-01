dbb5578a97c4603d57f4ca8a2db460787b3a1241
josvelema <volumensolutions@gmail.com>
2023-03-01 21:57:26 +0100

    edit general styles , buttons and paragraph alignments

--- galleryTry2.php
+++ galleryTry2.php
@@ -246,6 +246,19 @@ article.rj-gallery-card * + * {
     /* max-width: 800px; */
     /* max-height: 800px; */
 }
+
+.info-modal-pre {
+    color: #222;
+    line-height: 1.4;
+    font-size: 1.15rem;
+    max-width: 66ch;
+    padding: 1em;
+    background: #e0e0e0;
+    border-radius: 4px;
+    box-shadow: 0 2px 5px 5px #fff8;
+    border: 1px solid #2228;
+}
+
 .image-modal-content img {
     width: 97%;
     margin: 0 auto;
@@ -297,23 +310,27 @@ img {
 
 <main class="rj-black-bg-main">
 	<div class="content home">
+    <header>
+		<h1>Artworks Gallery</h1>
 
-		<h2>Gallery</h2>
 		<p> Follow Pieter’s development as a visual artist over time and place. Use the "view all’ button to get access to the whole data base and “category" field to make selections. New categories are under construction. </p>
-		<hr>
+		
 
 		<?php
 
 		?>
 		<?php if ($category != 'all') : ?>
-			<p>Viewing <?= $categories[array_search($category, array_column($categories, 'id'))]['title'] ?> (<?= $total_media ?> media files).</p>
+			<h2>Viewing <?= $categories[array_search($category, array_column($categories, 'id'))]['title'] ?> (<?= $total_media ?> media files).</h2>
 			<p><?= $categories[array_search($category, array_column($categories, 'id'))]['description'] ?> </p>
+      <hr>
 		<?php else : ?>
-			<p>Viewing all <?= $total_media ?> media files. (<?= $last_page ?> pages)</p>
+			<h2>Viewing all <?= $total_media ?> media files. (<?= $last_page ?> pages)</h2>
 		<?php endif; ?>
-
+    </header>
 		<div class="con">
-			<form action="" method="post"><input type="submit" value="View All" name="viewAll"></form>
+			<form action="" method="post"><input type="submit" value="View All" name="viewAll" class="rj-button">
+      <label for="viewAll"> from category</label>
+    </form>
 			<form action="" method="get">
 				<label for="category">Category:</label>
 				<select id="category" name="category" onchange="this.form.submit()">
@@ -412,6 +429,33 @@ img {
       </div>
     </article>
   <?php endforeach; ?>
+  </section>
+  <div class="pagination">
+			<?php if (!$viewingAll) {
+				if ($current_page > 1) : ?>
+
+					<a class="rj-prev" href="?page=<?= $current_page - 1 ?>&sort_by=<?= $sort_by ?>&category=<?= $category ?>">
+						<svg height="16" width="16" viewBox="0 0 1024 1024">
+							<path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path>
+						</svg>
+						<span>Prev</span>
+					</a>
+				<?php endif; ?>
+				<div class="rj-current-page">
+					Page <?= $current_page ?> of <?= $last_page ?>
+				</div>
+				<?php if ($current_page * $media_per_page < $total_media) : ?>
+					<a class="rj-next" href="?page=<?= $current_page + 1 ?>&sort_by=<?= $sort_by ?>&category=<?= $category ?>">
+						<span>Next</span>
+						<svg height="16" width="16" viewBox="0 0 1024 1024">
+							<path d="M 874.69 495.527 C 874.69 484.23 865.522 475.061 854.224 475.061 L 249.45 475.061 L 437.534 286.978 C 445.526 278.986 445.526 266.03 437.534 258.038 C 433.533 254.048 428.294 252.042 423.064 252.042 C 417.825 252.042 412.586 254.037 408.585 258.038 L 185.576 481.048 C 181.738 484.885 179.579 490.094 179.579 495.517 C 179.579 500.951 181.738 506.149 185.576 509.987 L 408.595 733.016 C 416.587 741.008 429.552 741.008 437.544 733.016 C 445.536 725.014 445.536 712.059 437.544 704.067 L 249.471 515.993 L 854.224 515.993 C 865.522 515.993 874.69 506.835 874.69 495.527 Z" transform="matrix(-1, 0, 0, -1, 1054.269043, 991.052002)"></path>
+						</svg>
+					</a>
+			<?php endif;
+			} else {
+				echo '<a href="#top">Back to top</a>';
+			} ?>
+		</div>
 
     <script>
 // Get modal elements
