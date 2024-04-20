aa2b97ce7c34443c674bfa424532303b2f850797
josvelema <volumensolutions@gmail.com>
2024-04-20 17:33:27 +0200

    factsheet v2.2

--- admin/js/generateFactSheet.js
+++ admin/js/generateFactSheet.js
@@ -1,6 +1,7 @@
 const FACT_CANVAS_WIDTH = 595;
 const FACT_CANVAS_HEIGHT = 842;
 
+
 const imageWidth = 400;
 const imageHeight = 300;
 
@@ -47,14 +48,26 @@ function createFactSheet(sheetData) {
     // ctx.lineWidth = 1;
     // ctx.strokeRect(10, 10, FACT_CANVAS_WIDTH - 20, FACT_CANVAS_HEIGHT - 20);
 
+    // current date in dd/mm/yyyy format
+    const date = new Date();
+    const dateString = `Artwork information sheet  ${date.getDate()}-${
+      date.getMonth() + 1
+    }-${date.getFullYear()}`;
+
+    // draw date text
+    ctx.font = "12px Times New Roman";
+    ctx.fillStyle = "#000000";
+    ctx.textAlign = "center";
+    ctx.fillText(dateString, x, 35);
+
     // draw title text
-    ctx.font = "24px Helvetica";
+    ctx.font = "18px Times New Roman";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "center";
 
     // wrap and draw title text
     const titleMaxWidth = FACT_CANVAS_WIDTH - 40;
-    fact_text_wrap(ctx, sheetData.title, x, 32, titleMaxWidth, 24);
+    fact_text_wrap(ctx, sheetData.title, x, 60, titleMaxWidth, 18);
     // draw main image
     const mainImg = new Image();
     mainImg.src = "../" + sheetData.imagePath;
@@ -73,24 +86,20 @@ function createFactSheet(sheetData) {
       }
 
       const centerImageX = (FACT_CANVAS_WIDTH - imageWidth) / 2;
-      ctx.drawImage(mainImg, centerImageX, 80, imageWidth, imageHeight);
+      ctx.drawImage(mainImg, centerImageX, 90, imageWidth, imageHeight);
       resolve(canvas.toDataURL("image/png"));
     };
 
     // draw border around image
-    ctx.strokeStyle = "#2a2a2a";
-    ctx.lineWidth = 3;
+    // ctx.strokeStyle = "#2a2a2a";
+    // ctx.lineWidth = 3;
     // set border left of the middle
-    let borderX = (FACT_CANVAS_WIDTH - imageWidth) / 2 - 15;
+    // let borderX = (FACT_CANVAS_WIDTH - imageWidth) / 2 - 15;
 
-    ctx.strokeRect(borderX, 60, 430, 330);
-
-    if (sheetData.description.length > 650) {
-      sheetData.description = sheetData.description.substring(0, 650) + "...";
-    }
+    // ctx.strokeRect(borderX, 60, 430, 330);
 
     // draw description text
-    ctx.font = "14px Helvetica";
+    ctx.font = "12px Times New Roman";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "left";
 
@@ -100,10 +109,10 @@ function createFactSheet(sheetData) {
     }
 
     // fact_text_wrap(ctx, sheetData.description, x, 420, 555, 20);
-    fact_text_wrap(ctx, sheetData.description, 40, 415, 550, 20);
+    fact_text_wrap(ctx, sheetData.description, 40, 415, 530, 16);
 
     // draw price text and format in euro currency
-    ctx.font = "18px Helvetica";
+    ctx.font = "12px Times New Roman";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "right";
 
@@ -112,47 +121,71 @@ function createFactSheet(sheetData) {
     const price = parseFloat(sheetData.art_price).toFixed(2);
     ctx.fillText(`Price: €${price}`, rightX, 540);
 
-    ctx.font = "16px Helvetica";
+    ctx.font = "12px Times New Roman";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "left";
-    ctx.fillText(
-      `Material: ${sheetData.art_material}`,
-      40,
-      545
-    );
-    ctx.font = "16px Helvetica";
+    ctx.fillText(`Material: ${sheetData.art_material}`, 40, 540);
+    ctx.font = "12px Times New Roman";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "left";
-    ctx.fillText(
-      `Dimensions: ${sheetData.art_dimensions}`,
-      40,
-      570
-    );
-    // add horizontal ruler
+    ctx.fillText(`Dimensions: ${sheetData.art_dimensions}`, 40, 570);
+    let disclaimer = `We ship all over the world. We take responsibility for the integrity of the work up to delivery at destination. Payment needs to be made in advance.  Various methods of payment are available. In case of loss of the work a full refund will be made. Conditions of shipment are dependent on the size and nature of the individual work.  The price of the work described in this factsheet is valid on the date of issue, but can be changed without notice. If you are interested please contact us: `;
+    ctx.font = "10px Times New Roman";
+    ctx.fillStyle = "#000000";
+    ctx.textAlign = "left";
+    fact_text_wrap(ctx, disclaimer, 40, 595, 530, 16);
 
-    ctx.beginPath();
-    ctx.moveTo(20, 580);
-    ctx.lineTo(575, 580);
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "#777";
-    ctx.stroke();
+    // add qr code
+    const qrImg = new Image();
+    // check if qr code exists
+    if (sheetData.qr_card_url == null) {
+      return;
+    } else {
+      qrImg.src = "../" + sheetData.qr_card_url;
 
-    // draw disclaimer image
-    const disclaimerImg = new Image();
-    disclaimerImg.src = "../assets/img/factSheetDisclaimer.png";
-    disclaimerImg.onload = () => {
-      ctx.drawImage(disclaimerImg, 20, 590, 551, 221);
-    };
+      qrImg.onload = () => {
+        ctx.drawImage(qrImg, 30, 670, 210, 140);
+      };
+    }
 
-    // draw year and fnr text
-    ctx.font = "16px Helvetica";
+    // contact details
+
+    let contact = `Adriaans & Van Kerchove, Lda.
+
+    Nif(BTW/Vat): 516357417   
+    
+    NIB: 0045.8061.40337554888.85
+    
+    IBAN:PT50 0045.8061.40337554888
+    
+    Tel: 0031 654234459 
+    
+    www.pieter-adriaans.com
+    
+    www.facebook.com/pieter.adriaans
+    
+    email: pieter@pieter-adriaans.com
+    `;
+    ctx.font = "10px Times New Roman";
     ctx.fillStyle = "#000000";
     ctx.textAlign = "center";
-    ctx.fillText(
-      `Year: ${sheetData.year} - follow up number: ${sheetData.fnr} - ID: ${sheetData.mediaId} `,
-      x,
-      826
-    );
+    fact_text_wrap(ctx, contact, x + 23, 690, 160, 16);
+
+    // logo kaasfabriek 
+    const logoImg = new Image();
+    logoImg.src = '../assets/img/kaasfabriekSmall.png';
+    logoImg.onload = () => {
+      ctx.drawImage(logoImg, x + 105 , 670, 165, 129);
+    };
+
+    // add horizontal ruler
+
+    // ctx.beginPath();
+    // ctx.moveTo(20, 580);
+    // ctx.lineTo(575, 580);
+    // ctx.lineWidth = 1;
+    // ctx.strokeStyle = "#777";
+    // ctx.stroke();
   });
 }
 
@@ -181,6 +214,7 @@ async function generateFactSheetAndSave(mediaId) {
     art_dimensions: mediaData.art_dimensions,
     art_type: mediaData.art_type,
     art_price: mediaData.art_price,
+    qr_card_url: mediaData.qr_card_url,
   };
 
   const imageDataUrl = await createFactSheet(cardData);
