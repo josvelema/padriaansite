fd8d89c28170e38a8444fcb50fe25e4168c74451
josvelema <volumensolutions@gmail.com>
2024-05-08 11:37:21 +0200

    pieterAPI origin http headers setup

--- pieterAPI.php
+++ pieterAPI.php
@@ -8,6 +8,25 @@
 include 'functions.php';
 // Connect to MySQL
 $pdo = pdo_connect_mysql();
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  // You can decide whether to allow the 'origin' in the 'Access-Control-Allow-Origin' header
+  // Replace '*' with the specific website you want to allow or keep it '*' to allow all websites
+  header("Access-Control-Allow-Origin: https://azart.codette.net/");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    header("Access-Control-Allow-Methods: GET, OPTIONS");
+
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+    header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+  exit(0);
+}
+
 
 // check the request method
 $method = $_SERVER['REQUEST_METHOD'];
